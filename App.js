b7f62303f642043cf39f2b2b864af61759436df6
ericleo8 <ericleonardo8399@gmail.com>
2023-07-29 12:55:37 +0700

    Guess The Country Timedoor

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import HomeScreen from './src/screens/HomeScreen';
+import GuessTheCountryScreen from './src/screens/GuessTheCountry';
+import WinScreen from './src/screens/WinScreen';
+import 'react-native-gesture-handler';
+import MainNavigator from './src/navigator/MainNavigator';
+const App = () => {
+  return (
+    <SafeAreaProvider>
+      <MainNavigator />
+    </SafeAreaProvider>
+  );
+};
+export default App;
---
+++ src/navigator/MainNavigator.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import GuessTheCountry from '../screens/GuessTheCountry';
+import WinScreen from '../screens/WinScreen';
+import GameOverScreen from '../screens/GameOverScreen';
+const Stack = createStackNavigator();
+const MainNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={GuessTheCountry}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Win"
+          component={WinScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="GameOver"
+          component={GameOverScreen}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+export default MainNavigator;
